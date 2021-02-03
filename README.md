5f5cecf44c373e444889c5a62a6c4e542ce9712d
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2021-02-03 15:44:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# pure-lazyload
+
+This is a custom element V1-based camera component.  
+
+This library works with the following browsers that support Intersection Observer.  
+
+![intersection-observer-support-browser.png](https://raw.githubusercontent.com/takuya-motoshima/pure-lazyload/master/screencap/intersection-observer-support-browser.png)
+
+## License
+
+[MIT licensed](./LICENSE.txt)
---
+++ types/LazyLoad.d.ts
@@ -0,0 +1,36 @@
+import './styles/lazyload.css';
+export default class {
+    /**
+     * Monitor if the target element is in the visible area.
+     */
+    private static observer;
+    /**
+     * Debug mode.
+     */
+    private static debugging;
+    /**
+     * Start lazy loading.
+     *
+     * @param {HTMLImageElement[]} imgs
+     * @param {{ root?: null|HTMLElement, rootMargin?: string, background?: string }} options
+     * @return {void}
+     */
+    static load(imgs: HTMLImageElement[], options?: {
+        root?: null | HTMLElement;
+        rootMargin?: string;
+        background?: string;
+        debugging?: boolean;
+    }): void;
+    /**
+     * Remove target element monitoring for lazy loading.
+     *
+     * @return {void}
+     */
+    static destroy(): void;
+    /**
+     * Output debug log.
+     *
+     * @param {any[]} .params
+     */
+    private static debug;
+}
